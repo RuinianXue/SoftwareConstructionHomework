b097a2028e5b27144f3a6d42a9b7cd7bde5dcac6
RuinianXue <92255709+RuinianXue@users.noreply.github.com>
2023-03-08 08:15:20 +0800

    Replace

---
+++ HW2/CheckMatrix/CheckMatrix/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Matrix
+{
+    public int[][] a;
+    public int n, m;
+    public Matrix()
+    {
+        a = new int[2][];
+        n=m = 0;
+    }
+    public Matrix(int n, int m)
+    {
+        this.n = n;
+        this.m = m;
+        a = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            string s=Console.ReadLine();
+            int j = 0,pos=0;
+            a[i]=new int[m];
+            while (pos < s.Length)
+            {
+                if (s[pos] <= '9' && s[pos]>='0')
+                {
+                    int num = 0;
+                    while(pos<s.Length&& s[pos] <= '9' && s[pos] >= '0')
+                    {
+                        num = num * 10 + (int)(s[pos]-'0');
+                        pos++;
+                    }
+                    
+                    a[i][j] = num;
+                    //Console.WriteLine(i+" "+j);
+                    j++;
+                }
+               else pos++;
+            }
+        }
+    }
+    public void outAns()
+    {
+        for (int i = 0;i < n;i++)
+        {
+            for(int j = 0;j < m;j++)
+            {
+                Console.Write(a[i][j]+" ");
+            }
+            Console.Write("\n");
+        }
+    }
+    public bool checkMatrix()
+    {
+        for(int i= 0;i < m; i++)
+        {
+            for(int j = 1;j < n&&(i+j<m); j++)
+            {
+                if (a[0][i] != a[j][i+j]) return false;
+            }
+        }
+        return true;
+    }
+}
+
+namespace CheckMatrix
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("请用两行分别键入矩阵的长和宽");
+            int n, m;
+            n=int.Parse(Console.ReadLine());
+            m=int.Parse(Console.ReadLine());
+            Console.WriteLine("请键入矩阵");
+            Matrix m1 = new Matrix(n,m);
+            m1.outAns();
+            Console.WriteLine(m1.checkMatrix());
+        }
+    }
+}
+/*
+2
+3
+1 2 3
+4 1 1
+ */
---
+++ HW2/ListOfNumbers/ListOfNumbers/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+class List
+{
+    public int n;
+    public int[] a;
+    public List()
+    {
+        a=new int[0];
+        n = 0;
+    }
+    public List(int num)
+    {
+        n = num;
+        a=new int[num+1];
+        Console.WriteLine("请在此键入数组，一行一个数字");
+        for(int i=1;i<=n;i++)
+        {
+            a[i] = int.Parse(Console.ReadLine());
+        }
+    }
+    public void FindKey()
+    {
+        int sum = 0;
+        int avg = 0;
+        int maxx = -0xfffffff;
+        int minn = 0xfffffff;
+        for(int i=1;i<=n; i++)
+        {
+            sum += a[i];
+            maxx = (a[i] > maxx) ? a[i] : maxx;
+            minn = (a[i]<minn) ? a[i] : minn;
+        }
+        avg = sum / n;
+        Console.WriteLine("这串数字的和是: "+sum+"\n这串数字的平均值是: "+avg+"\n这段数字的最小值和最大值分别是: "+minn+" "+maxx+"\n");
+    }
+}
+namespace ListOfNumbers
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            int n;
+            Console.WriteLine("请在此键入数组的元素个数");
+            n= int.Parse(Console.ReadLine());
+            List listt = new List(n);
+            listt.FindKey();
+        }
+    }
+}
