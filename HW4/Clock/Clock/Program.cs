2bb0990d11b5b9436a1f6f567e63c541284f2a30
RuinianXue <92255709+RuinianXue@users.noreply.github.com>
2023-03-21 12:37:07 +0800

    HW4

---
+++ HW4/Clock/Clock/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Clock
+{
+    public event EventHandler Tick;
+    public event EventHandler Alarm;
+    public DateTime AlarmTime { get; set; }
+
+    public Clock(DateTime alarmTime)
+    {
+        this.AlarmTime = alarmTime;
+        System.Timers.Timer timer = new System.Timers.Timer(1000);
+        timer.Elapsed += (sender, e) =>
+        {
+            OnTick(); 
+
+            if (DateTime.Now >= AlarmTime) 
+            {
+                OnAlarm();
+                timer.Stop();
+            }
+        };
+        timer.Start();
+    }
+    public virtual void OnTick()
+    {
+        if (Tick != null)
+        {
+            Tick(this, EventArgs.Empty);
+            Console.WriteLine("Tick tick..."); 
+        }
+
+    }
+    public virtual void OnAlarm()
+    {
+        if (Alarm != null)
+        {
+            Alarm(this, EventArgs.Empty); 
+            Console.WriteLine("It rings！"); 
+        }
+
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while(true)
+        {
+            System.Console.WriteLine("Please set your alarm time. (s)");
+            int aT=5;//default time is 5seconds
+            try
+            {
+                aT = int.Parse(System.Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            DateTime alarmTime = DateTime.Now.AddSeconds(aT);
+            Clock clock = new Clock(alarmTime);
+            clock.Tick += Clock_Tick;
+            clock.Alarm += Clock_Alarm;
+            Console.ReadLine();
+        }
+    }
+    private static void Clock_Tick(object sender, EventArgs e)
+    {
+        Console.WriteLine("Now Time：{0}", DateTime.Now); 
+    }
+
+    private static void Clock_Alarm(object sender, EventArgs e)
+    {
+        Console.WriteLine("DiDiDi！");
+    }
+}
---
+++ HW4/LinkList/LinkList/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Node<T>
+{
+    public T data; 
+    public Node<T> next; 
+    public Node(T data) 
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LinkedList<T>
+{
+    public Node<T> head;
+    public int count;
+
+    public LinkedList() 
+    {
+        this.head = null;
+        this.count = 0;
+    }
+
+    public void AddLast(T data)
+    {
+        Node<T> newNode = new Node<T>(data); 
+        if (head == null) 
+        {
+            head = newNode;
+        }
+        else  
+        {
+            Node<T> current = head;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        count++; 
+    }
+
+    public void ForEach(Action<T> action)
+    {
+        Node<T> current = head;
+        while (current != null)
+        {
+            action(current.data); 
+            current = current.next;
+        }
+
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> list = new LinkedList<int>();  
+        list.AddLast(1);  
+        list.AddLast(2);
+        list.AddLast(3);
+        list.AddLast(4);
+        int sum = 0;  
+        list.ForEach(x => Console.WriteLine(x));  
+        list.ForEach(x => sum += x); 
+        Console.WriteLine("Sum: " + sum);
+        int max = int.MinValue;  
+        list.ForEach(x => { if (x > max) max = x; }); 
+        Console.WriteLine("Max: " + max);
+        int min = int.MaxValue;  
+        list.ForEach(x => { if (x < min) min = x; }); 
+        Console.WriteLine("Min: " + min);
+    }
+}
